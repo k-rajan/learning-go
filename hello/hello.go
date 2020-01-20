d881e752d559edae98c124dd3822cf6d390e8ac9
Rajan <kkumar.rajan@gmail.com>
2020-01-20 15:11:47 +0530

    hello world multi language red-green-refactor

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,32 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const enHelloPrefix = "Hello, "
+const frHelloPrefix = "Bonjour, "
+const esHelloPrefix = "Hola, "
+
 // Hello somthing
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = esHelloPrefix
+	case french:
+		prefix = frHelloPrefix
+	default:
+		prefix = enHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,10 +5,36 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("rajan")
-	want := "Hello, rajan"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMsg := func(t *testing.T, got string, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
 	}
+
+	t.Run("saying hello by name", func(t *testing.T) {
+		got := Hello("rajan", "English")
+		want := "Hello, rajan"
+		assertCorrectMsg(t, got, want)
+	})
+
+	t.Run("saying hello world when empty", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMsg(t, got, want)
+	})
+
+	t.Run("saying hello in spanish", func(t *testing.T) {
+		got := Hello("Amigo", "Spanish")
+		want := "Hola, Amigo"
+		assertCorrectMsg(t, got, want)
+	})
+	t.Run("saying hello in french", func(t *testing.T) {
+		got := Hello("Cornor", "French")
+		want := "Bonjour, Cornor"
+		assertCorrectMsg(t, got, want)
+	})
 }
