f7c8a7af865f1b0e20731a9f1b8a81f9debbec72
Rajan <kkumar.rajan@gmail.com>
2020-02-21 15:58:42 +0530

    context

---
+++ context/context.go
@@ -0,0 +1,26 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// Store fetches
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+// Server returns handler
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+		fmt.Fprint(w, data)
+
+	}
+}
